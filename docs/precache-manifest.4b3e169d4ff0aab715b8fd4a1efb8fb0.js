a60fa90aeda193d6bde6605a171df2a500802808
ebubello <ebubello@gmail.com>
2020-09-27 21:34:25 -0300

    Docs Add

---
+++ docs/precache-manifest.4b3e169d4ff0aab715b8fd4a1efb8fb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c353ff9ba585c542abfe8023c1da7e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef6de87a35778546a8cf",
+    "url": "/static/css/main.14324acd.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef6de87a35778546a8cf",
+    "url": "/static/js/main.7bb863a1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
